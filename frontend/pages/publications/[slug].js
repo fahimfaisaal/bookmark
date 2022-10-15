531e523767a12f32198777b655645f49d8ee17a6
jahid-bd <jahidbd9x@gmail.com>
2022-10-15 11:50:33 +0600

    config(folder): create folder structure

---
+++ frontend/pages/publications/[slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PublicationItem = () => {
+  return (
+    <div>
+      <h1>Publication Item Page</h1>
+    </div>
+  );
+};
+
+export default PublicationItem;
---
+++ frontend/pages/shop/index.js
@@ -0,0 +1,9 @@
+const Shop = () => {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+    </div>
+  );
+};
+
+export default Shop;
