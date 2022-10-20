b35aa224127ce26cafe8dac3416e76dd9f0bdbf3
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2022-10-20 10:26:46 +0600

    feat: update home page content

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+### PG SETUP
+
+- download & setup PGADMIN [watch video](https://www.youtube.com/watch?v=FeZyCxwb1aQ)
+- create db `bookmark`
+- strapi backend install `npm i pg`
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,16 @@
-import {
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  Divider,
-  Switch,
-  Typography,
-  useTheme,
-} from '@mui/material';
+import { Container, CssBaseline, Switch, useTheme } from '@mui/material';
+import AddressCard from '../components/AddressCard';
 import AuthorCard from '../components/AuthorCard';
+import AuthorProfile from '../components/AuthorProfile';
+import BookCard from '../components/BookCard';
 import CartItem from '../components/CartItem';
 import CategoryCard from '../components/CategoryCard';
+import LocationIcon from '../components/Icons/Location';
+import SocialMediaIcons from '../components/Icons/SocialMedia';
 import PublicationCard from '../components/PublicationCard';
-import RecipeReviewCard from '../components/test/Card';
+import ReviewCard from '../components/ReviewCard';
 import { UseThemeContext } from '../context/ThemeContext';
 import ComponentsOverrides from '../src/theme/overrides';
-import myShadows from '../src/theme/shadows';
 import ResponsiveAppBar from './testMUI';
 
 export default function Home() {
@@ -40,106 +35,15 @@ export default function Home() {
         checked={mode === 'dark'}
         onChange={() => handleChangeMode()}
       />
+      {/* ==========Done by Rezuan========= */}
+      <AuthorProfile />
+      <LocationIcon />
+      <SocialMediaIcons />
+      <BookCard />
+      <AddressCard />
+      <ReviewCard />
 
-      <Button variant="btnDark">Demo</Button>
-
-      <Button variant="btnLight"></Button>
-
-      <Typography variant="h1">Heading - 1</Typography>
-      <Typography variant="h2" sx={{}}>
-        {' '}
-        Popular Products{' '}
-      </Typography>
-      <Typography variant="h3" sx={{ color: 'text.disabled' }}>
-        {' '}
-        Heading - 3{' '}
-      </Typography>
-      <Typography variant="caption" sx={{ color: 'text.secondary' }}>
-        {' '}
-        My First Caption{' '}
-      </Typography>
-      <Typography variant="body2" sx={{ color: 'text.primary' }}>
-        The BNP alleged that a transportation strike, which was not declared
-        earlier, was enforced in Mymensingh division ahead of the rally in order
-        to to prevent BNP men joining the rally. Sensing that such obstructions
-        might come, BNP leaders and activists took up various strategies,
-        including being at the venue well before the rally began at 2:00pm,
-        party insiders said. Many of them stayed in Mymensingh overnight to
-        dodge the ruling party men and the law enforcement agencies, they added.
-        In the absence of transport, party men reached the venue on boats and
-        trains. On Friday night, many BNP activists were seen sitting or sleep
-        at the venue, while others made their sleeping arrangements on a
-        footpath nearby.
-      </Typography>
-      <Divider sx={{ m: 4 }} />
-      <Button variant="btnDark" sx={{ m: 2 }}>
-        Become a Seller{' '}
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ backgroundColor: 'primary.light', m: 2 }}
-      >
-        Primary Light
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ backgroundColor: 'primary.dark', m: 2 }}
-      >
-        Join
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ backgroundColor: 'secondary.main', m: 2 }}
-      >
-        Secondary Main
-      </Button>
-      <Button variant="btnLight">English</Button>
-      <Button variant="contained" sx={{ backgroundColor: 'secondary.dark' }}>
-        Secondary Dark
-      </Button>
-      <Button
-        variant="contained"
-        sx={{ backgroundColor: 'background.paper', color: 'text.primary' }}
-      >
-        Category{' '}
-      </Button>
-      <Box
-        sx={{
-          boxShadow: '1',
-          backgroundColor: 'background.paper',
-          width: '8rem',
-          height: '5rem',
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          p: 1,
-          m: 1,
-          borderRadius: 2,
-          textAlign: 'center',
-          fontSize: '0.875rem',
-          fontWeight: '700',
-        }}
-      >
-        boxShadow: 1
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: 'common.white',
-          boxShadow: myShadows.box,
-          width: '8rem',
-          height: '5rem',
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          p: 1,
-          m: 1,
-          borderRadius: 2,
-          textAlign: 'center',
-          fontSize: '0.875rem',
-          fontWeight: '700',
-        }}
-      >
-        boxShadow: 2
-      </Box>
-      <RecipeReviewCard />
+      {/* ==========Done by Rezuan========= */}
       <CategoryCard />
       <CartItem />
       <AuthorCard />
