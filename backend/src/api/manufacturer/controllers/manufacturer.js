7c525830c5cc902bccd63b11a2bf43b69b0d3f7c
nahidnstu12 <nahid.dev19@gmail.com>
2022-11-09 20:52:32 +0600

    fix:auth modal default state

--- backend/src/api/manufacturer/controllers/manufacturer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * manufacturer controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::manufacturer.manufacturer');
