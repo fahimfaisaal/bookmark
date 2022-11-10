b56b73527aba84966d4bab1aed18703340d06f27
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-11-10 11:41:35 +0600

    chore: remove useless cloudinary setup

--- backend/config/env/development/plugins.js
+++ backend/config/env/development/plugins.js
@@ -1,25 +1,11 @@
 /* eslint-disable linebreak-style */
-module.exports = ({ env }) =>
-  Object.assign(
-    {
-      'users-permissions': {
-        config: {
-          jwt: {
-            expiresIn: '7d'
-          }
-        }
-      }
-    },
-    process.env.NODE_ENV === 'production' && {
-      upload: {
-        config: {
-          provider: 'cloudinary',
-          providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME'),
-            api_key: env('CLOUDINARY_KEY'),
-            api_secret: env('CLOUDINARY_SECRET')
-          }
+module.exports = () =>
+  Object.assign({
+    'users-permissions': {
+      config: {
+        jwt: {
+          expiresIn: '7d'
         }
       }
     }
-  );
+  });
