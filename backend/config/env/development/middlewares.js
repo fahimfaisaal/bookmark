e4f37d092e44420de40df9a011c57f99dcc82345
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2022-11-10 12:02:08 +0600

    chore: remove useless cloudinary middleware

--- backend/config/env/development/middlewares.js
+++ backend/config/env/development/middlewares.js
@@ -3,32 +3,6 @@
 /* eslint-disable linebreak-style */
 module.exports = [
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'res.cloudinary.com'
-          ],
-          'media-src': [
-            "'self'",
-            'data:',
-            'blob:',
-            'dl.airtable.com',
-            'res.cloudinary.com'
-          ],
-          upgradeInsecureRequests: null
-        }
-      }
-    }
-  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
--- backend/config/env/production/middlewares.js
+++ backend/config/env/production/middlewares.js
@@ -10,8 +10,20 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com'
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'res.cloudinary.com'
+          ],
           upgradeInsecureRequests: null
         }
       }
