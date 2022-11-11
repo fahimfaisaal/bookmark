d2756fc44016ad7778425a97dd2e6cf352d17d03
Developer <fahimfaisal.soikot@gmail.com>
2022-11-11 09:17:51 +0600

    chore(dev): include dotenv vault & renamed production envs

--- backend/config/env/production/admin.js
+++ backend/config/env/production/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET')
+    secret: env('ADMIN_JWT_SECRET_PRODUCTION')
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT')
+    salt: env('API_TOKEN_SALT_PRODUCTION')
   }
 });
