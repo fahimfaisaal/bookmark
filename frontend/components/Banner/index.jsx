6e8e19ca20763e101198e20eea39ec24d58efa27
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2022-11-15 06:41:08 +0600

    fix: package json and banner image resolve

--- frontend/components/Banner/index.jsx
+++ frontend/components/Banner/index.jsx
@@ -5,37 +5,40 @@ import { StyledGrid, StyledGridItem, StyledText } from './Styles';
 import Link from 'next/link';
 
 const Banner = ({ bannerData }) => {
+  console.log('BannerData', bannerData);
   return (
     <StyledGrid container>
-      <StyledGridItem item md={6} lg={6}>
-        {bannerData?.map((myData) => (
-          <Stack
-            direction={'column'}
-            alignItems={'flex-start'}
-            spacing={2}
-            key={myData.id}
-          >
-            <Typography variant="h4">{myData?.status}</Typography>
-            <StyledText variant="h1" fontSize={'3rem'}>
-              {myData?.heading.slice(0, 14)} <br />
-              {myData?.heading.slice(14, 28)}
-            </StyledText>
-            {myData?.buttons?.map((item) => (
-              <Link key={item.id} href={item.url}>
-                <Button sx={{ color: 'white' }}>
-                  {item.text}
-                  <KeyboardDoubleArrowRightIcon />
-                </Button>
-              </Link>
-            ))}
-          </Stack>
-        ))}
-      </StyledGridItem>
-      <Grid item md={6} lg={6}>
-        <Box>
-          <img src="/banner.png" width="100%" alt={data.attributes?.heading} />
-        </Box>
-      </Grid>
+      {bannerData?.map((myData) => (
+        <>
+          <StyledGridItem item md={6} lg={6}>
+            <Stack
+              direction={'column'}
+              alignItems={'flex-start'}
+              spacing={2}
+              key={myData.id}
+            >
+              <Typography variant="h4">{myData?.status}</Typography>
+              <StyledText variant="h1" fontSize={'3rem'}>
+                {myData?.heading.slice(0, 14)} <br />
+                {myData?.heading.slice(14, 28)}
+              </StyledText>
+              {myData?.buttons?.map((item) => (
+                <Link key={item.id} href={item.url}>
+                  <Button sx={{ color: 'white' }}>
+                    {item.text}
+                    <KeyboardDoubleArrowRightIcon />
+                  </Button>
+                </Link>
+              ))}
+            </Stack>
+          </StyledGridItem>
+          <Grid item md={6} lg={6}>
+            <Box>
+              <img src="/banner.png" width="100%" alt={myData?.heading} />
+            </Box>
+          </Grid>
+        </>
+      ))}
     </StyledGrid>
   );
 };
